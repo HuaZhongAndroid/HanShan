2bad3ea963ea6764de5b7b50981d4cc01e52d8cf
jpmac <jpmac@jpmac.local>
2018-05-30 15:07:11 +0800

    增加保存图片到相册

--- tongzijunyh/src/com/bm/tzj/activity/CourseWebActivity.java
+++ tongzijunyh/src/com/bm/tzj/activity/CourseWebActivity.java
@@ -1,9 +1,13 @@
 package com.bm.tzj.activity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,6 +19,9 @@ import com.bm.share.ShareModel;
 import com.bm.tzj.kc.PayInfoAc3;
 import com.richer.tzj.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -141,4 +148,65 @@ public class CourseWebActivity extends AbsCoursePayBaseAc implements OnClickList
         }
         return null;
     }
+
+
+
+
+
+
+    /**
+     * 保存图片到相册
+     * @param bmp 获取的bitmap数据
+     * @param picName 自定义的图片名
+     */
+    public static void saveBmp2Gallery(Context ctx, Bitmap bmp, String picName) {
+
+        String fileName = null;
+        //系统相册目录
+        String galleryPath= Environment.getExternalStorageDirectory()
+                + File.separator + Environment.DIRECTORY_DCIM
+                +File.separator+"Camera"+File.separator;
+
+
+        // 声明文件对象
+        File file = null;
+        // 声明输出流
+        FileOutputStream outStream = null;
+
+        try {
+            // 如果有目标文件，直接获得文件对象，否则创建一个以filename为名称的文件
+            file = new File(galleryPath, picName+ ".jpg");
+
+            // 获得文件相对路径
+            fileName = file.toString();
+            // 获得输出流，如果文件中有内容，追加内容
+            outStream = new FileOutputStream(fileName);
+            if (null != outStream) {
+                bmp.compress(Bitmap.CompressFormat.JPEG, 90, outStream);
+            }
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }finally {
+            try {
+                if (outStream != null) {
+                    outStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        //通知相册更新
+        MediaStore.Images.Media.insertImage(ctx.getContentResolver(),
+                bmp, fileName, null);
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        ctx.sendBroadcast(intent);
+
+
+    }
+
+
 }
